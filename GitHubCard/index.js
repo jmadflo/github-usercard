cce1a80f8449e8153a49c9734131bf1cac8a3df0
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-03-12 14:58:07 -0600

    personal profile card rendered

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -35,7 +35,10 @@ axios.get('https://api.github.com/users/jmadflo')
 const followersArray = [];
 
 axios.get('https://api.github.com/users/jmadflo/followers')
-  .then()
+  .then(response => {
+    console.log(response)
+    
+  })
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
